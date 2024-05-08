3a4e17466dcc4528239e1ec5f63b590b9b752d8e
Alessandro Nardecchia <{ID}+{username}@users.noreply.github.com>
2024-05-08 13:50:29 +0000

    Added new postgres and ns8-odoo images: 16.20240508

--- build-images.sh
+++ build-images.sh
@@ -3,8 +3,8 @@
 # Terminate on error
 set -e
 
-POSTGRES_VERSION="16.20240503.1"
-ODOO_VERSION="16.20240424"
+POSTGRES_VERSION="16.20240508"
+ODOO_VERSION="16.20240508"
 
 # Prepare variables for later use
 images=()
